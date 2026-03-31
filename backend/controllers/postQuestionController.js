a26ed08ec15b2ac6e99ce6a8b8b720d131af692a
Krish Shah <krishcshah09@gmail.com>
2026-03-31 10:10:36 +0530

    added validation to post questions

--- backend/controllers/postQuestionController.js
+++ backend/controllers/postQuestionController.js
@@ -1,12 +1,26 @@
 import pool from '../db.js'
 export default async function(req,res){
-    const {title,body,author_name}=req.body
-    const result=await pool.query(`INSERT INTO questions(
-            title,body,author_name
+    let {title,body,author_name}=req.body
+    title=title.trim()
+    body=body.trim()
+    author_name=author_name.trim()
+    if(!title||!body||!author_name){
+        res.status(400)
+        res.json({error:"Missing required parameters. All the title,body and author_name are required params"})
+        return
+    }
+    try{
+        const result=await pool.query(`INSERT INTO questions(
+                title,body,author_name
+            )
+            VALUES($1,$2,$3)
+            RETURNING *;`,
+            [title, body, author_name]
         )
-        VALUES($1,$2,$3);`,
-        [title, body, author_name]
-    )
-    res.status(200)
-    res.json(result.rows)
+        res.status(201)
+        res.json(result.rows)
+    }catch(err){
+        console.error(err)
+        res.status(500).send("DB connection failed")
+    }
 }
