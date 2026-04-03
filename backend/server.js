92e4c128e021fe379d9f6167b90e11d66dad8827
Krish Shah <krishcshah09@gmail.com>
2026-04-03 16:17:24 +0530

    added cors for only frontend to access api

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ import {router} from './routers/router.js'
 
 const app=express()
 const PORT=8000
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://doubt-solver-pi.vercel.app"
+  ]
+}))
 app.use(express.json())
 app.use('/',router)
 
