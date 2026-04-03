83a613486e149d989437bf1f90880fb02a39074c
Krish Shah <krishcshah09@gmail.com>
2026-04-03 11:35:31 +0530

    backend ready for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 import pkg from 'pg'
 const {Pool}=pkg
-const pool=new Pool({
-    user: 'krishshah',
-    host: 'localhost',
-    database: 'doubt_solver',
-    password: '',
-    port: 5432,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 export default pool
